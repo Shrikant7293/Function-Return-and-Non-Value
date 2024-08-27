4195acbfa9f5c7ac107dbd8a0a665aba3af2fc9a
Shrikant Pawar <shrikantpawar703@gmail.com>
2024-08-27 00:24:28 -0700

    Add project files.

---
+++ Function.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Function_Return_and_Non_Value
+{
+	internal class Function
+	{
+		public int Id { get; set; }
+        public string Name { get; set; }
+        public int GrossIncome { get; set; }
+        public int IncomeTax {  get; set; }
+		public int NetIncome {  get; set; }
+
+		public void GetData()
+		{
+			Console.WriteLine("Enter Id");
+			Id = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Enter Name");
+			Name = Console.ReadLine();
+
+			Console.WriteLine("Enter GrossIncome");
+			GrossIncome= Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Enter IncomeTax");
+			IncomeTax= Convert.ToInt32(Console.ReadLine());
+
+			NetIncome = CalculateNetIncome();
+		}
+
+		public void PutData()
+		{
+			Console.WriteLine("Id = " + Id);
+			Console.WriteLine("Name = " + Name);
+			Console.WriteLine("GrossIncome = " + GrossIncome);
+			Console.WriteLine("IncomeTax = " + IncomeTax);
+			Console.WriteLine("NetIncome = " + NetIncome);
+		}
+
+		public int CalculateNetIncome()
+		{
+			NetIncome = GrossIncome - IncomeTax;
+			
+			return NetIncome;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Function_Return_and_Non_Value;
+
+
+class Program
+{
+	public static void Main()
+	{
+
+		Function function = new Function();
+		function.GetData();
+		Console.WriteLine(" ");
+		function.PutData();
+
+	}
+}
